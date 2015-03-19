b0818e267276611e30c02d6036254fe518d1377d
Ian Blakley <ian@810labs.me>
2015-03-18 20:01:16 -0400

    Removed the action menu in the controller activity

--- app/src/main/java/me/eighttenlabs/toggle/ControllerActivity.java
+++ app/src/main/java/me/eighttenlabs/toggle/ControllerActivity.java
@@ -1,18 +1,16 @@
 package me.eighttenlabs.toggle;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.Toast;
 
 
-public class ControllerActivity extends ActionBarActivity implements View.OnTouchListener {
+public class ControllerActivity extends Activity implements View.OnTouchListener {
 
     MouseControl mControl;
     TypeBox text;
@@ -105,28 +103,6 @@ public class ControllerActivity extends ActionBarActivity implements View.OnTouc
         });
         findViewById(R.id.button_click_left).setOnTouchListener(this);
         findViewById(R.id.button_click_right).setOnTouchListener(this);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     public class ConnectTask extends AsyncTask<String, String, Sender> {
